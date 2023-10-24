59d636998bc364f089ea1b28812d67fba04c8dcc
Manzi Tresor <manzitre@gmail.com>
2023-10-24 08:46:16 +0200

    Revert "Hello Rails-React: Features"

--- app/controllers/greetings_controller.rb
+++
@@ -1,9 +0,0 @@
-class GreetingsController < ApplicationController
-  def index
-    @greetings = Greeting.all
-    respond_to do |format|
-      format.html { render 'index' }
-      format.json { render json: @greetings }
-    end
-  end
-end
